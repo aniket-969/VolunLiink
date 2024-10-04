3e3042ffc4d8586a54bbb4cefd3c6f3ed4408eff
Aniket <aniketbaranwal8090@gmail.com>
2024-10-04 21:52:34 +0530

    user router code formatted

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -7,12 +7,12 @@ import {
   refreshTokens,
   changePassword,
   updateAccountDetails,
-  updateUserAvatar
+  updateUserAvatar,
 } from "../controllers/user.controller.js";
 import { upload } from "./../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { validate } from "../middlewares/validator.middleware.js";
-  import { loginSchema, userSchema } from "../../schema/UserSchema.js"; 
+import { loginSchema, userSchema } from "../../schema/UserSchema.js";
 
 const router = Router();
 
@@ -26,19 +26,21 @@ router.route("/register").post(
   validate(userSchema),
   registerUser
 );
- 
-router.route("/login").post(validate(loginSchema),loginUser);
+
+router.route("/login").post(validate(loginSchema), loginUser);
 
 router.route("/logout").post(verifyJWT, logoutUser);
 
-router.route("/refreshTokens").post(refreshTokens)
+router.route("/refreshTokens").post(refreshTokens);
 
-router.route("/change-password").post(verifyJWT,changePassword)
+router.route("/change-password").post(verifyJWT, changePassword);
 
-router.route("/update-account").patch(verifyJWT,updateAccountDetails)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+ 
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
 
-router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
-
-router.route("/user").get(verifyJWT,getUserDetails);
+router.route("/user").get(verifyJWT, getUserDetails);
 
 export default router;
