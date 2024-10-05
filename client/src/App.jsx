68fec3f0b8a070541d4c740656d54a5e7623ada8
Aniket <aniketbaranwal8090@gmail.com>
2024-10-05 21:09:22 +0530

    Removed tokenChecker component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,38 +4,32 @@ import RootLayout from "./pages/root/RootLayout";
 import Home from "./pages/root/Home";
 import SigninForm from "./pages/auth/form/SigninForm";
 import SignupForm from "./pages/auth/form/SignupForm";
-import './App.css'
+import "./App.css";
 import Profile from "./pages/root/Profile";
 import PostDetails from "./components/PostDetails";
 import CreatePost from "./pages/root/CreatePost";
-import { Toaster } from 'react-hot-toast'
-import TokenChecker from "./components/TokenChecker";
+import { Toaster } from "react-hot-toast";
 import NotFound from "./components/NotFound";
 
 function App() {
-
   return (
     <>
-      {/* <TokenChecker /> */}
       <Routes>
-
         <Route element={<AuthLayout />}>
           <Route path="/sign-in" element={<SigninForm />} />
           <Route path="/sign-up" element={<SignupForm />} />
         </Route>
         <Route index element={<Home />} />
         <Route element={<RootLayout />}>
-
           <Route path="/profile/:id/*" element={<Profile />} />
           <Route path="/create-post" element={<CreatePost />} />
-
         </Route>
         <Route path="/posts/:postId" element={<PostDetails />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Toaster position="top-right" />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
